d419de1b31b5cc1372a12bd61c969cd804b02d64
Ilya <i.galjatdinov@t-code.ru>
2024-06-04 13:17:25 +0500

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// Галерея и лайтбоксы от Fancybox
+import { Fancybox } from '@fancyapps/ui'
+import '@fancyapps/ui/dist/fancybox/fancybox.css'
+
+Fancybox.bind('[data-fancybox]', {
+	// Your custom options
+});
